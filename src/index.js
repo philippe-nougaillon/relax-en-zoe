8b043a55ac841ac0b6273af8f9814afb97acb1e8
philippe-nougailon <philippe.nougaillon@gmail.com>
2018-12-19 11:52:50 +0100

    calculate!

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
-//import './index.css';
+import './index.css';
 //import * as serviceWorker from './serviceWorker';
 
 
@@ -11,12 +11,15 @@ import { createLogger } from 'redux-logger';
 const SPEED_UP = 'SPEED_UP';
 const SPEED_DOWN = 'SPEED_DOWN';
 
+const TEMP_UP = 'TEMP_UP';
+const TEMP_DOWN = 'TEMP_DOWN';
 
 // reducers
 
 const params = {
     speed: 90,
-    autonomie: 300,
+    autonomie: 253,
+    temp: 0,
 };
 
 function paramsReducer(state = params, action) {
@@ -27,18 +30,54 @@ function paramsReducer(state = params, action) {
         case SPEED_DOWN: {
             return applySpeedDOWN(state, action);
         }
+        case TEMP_UP: {
+            return applyTempUP(state, action);
+        }
+        case TEMP_DOWN: {
+            return applyTempDOWN(state, action);
+        }
         default: return state;
     }
 }
 
 function applySpeedUP(state, action) {
-    const newspeed = action.params.speed + 10;
-    return {...state, speed: newspeed};
+    const newSpeed = action.params.speed + 10;
+    const newAutonomie = calculate(100, newSpeed, 0);
+
+    if (newAutonomie)
+        return {...state, speed: newSpeed, autonomie: newAutonomie};
+    else
+        return state;
 }
 
 function applySpeedDOWN(state, action) {
-    const newspeed = action.params.speed - 10;
-    return {...state, speed: newspeed};
+    const newSpeed = action.params.speed - 10;
+    const newAutonomie = calculate(100, newSpeed, 0);
+
+    if (newAutonomie)
+        return {...state, speed: newSpeed, autonomie: newAutonomie};
+    else
+        return state;
+}
+
+function applyTempUP(state, action) {
+    const newTemp = action.params.temp + 10;
+    const newAutonomie = calculate(100, action.params.speed, newTemp);
+
+    if (newAutonomie)
+        return {...state, temp: newTemp, autonomie: newAutonomie};
+    else
+        return state;
+}
+
+function applyTempDOWN(state, action) {
+    const newTemp = action.params.temp - 10;
+    const newAutonomie = calculate(100, action.params.speed, newTemp);
+
+    if (newAutonomie)
+        return {...state, temp: newTemp, autonomie: newAutonomie};
+    else
+        return state;
 }
 
 
@@ -56,6 +95,45 @@ function doSpeedDOWN(params) {
         type: SPEED_DOWN,
         params,
     };
+}
+
+function doTempUP(params) {
+    return {
+        type: TEMP_UP,
+        params,
+    };
+}
+
+function doTempDOWN(params) {
+    return {
+        type: TEMP_DOWN,
+        params,
+    };
+}
+
+
+// Calculations 
+    
+function calculate(charge, speed, temp) {
+
+    const consommations = { '50': 5.35, '60': 6.83, '70': 8.83, '80': 11.12, '90': 13.82, '100': 17.75, '110': 22.22, '120': 27.33, '130': 32.7 };
+    const températures  = { '30': -2.5, '20': 0, '10': 2.5, '0': 5, '-10': 7.5, '-20': 10};
+    
+    // Puissance restante
+    const puissance = 41; // Batterie ZOE 4.0 (41kW)
+    const battery   = puissance - (puissance * (100 - charge) / 100);
+
+    // Consommation 
+    const conso = consommations[speed];
+    let autonomie = battery * (parseInt(speed) / conso);
+
+    // Impact de la température extérieure
+    const impact = températures[temp];
+    autonomie  = autonomie - (autonomie * impact / 100); 
+
+    //console.log("Charge:" + charge, "% | Speed:" + speed + " | Temp:" + temp + "° | Autonomie:" + autonomie);
+
+    return autonomie;
 }
 
 
@@ -78,25 +156,46 @@ const store = createStore(
 
 // View layer
 
-function TheApp({ params, onSpeedDOWN, onSpeedUP }) {
-    const { speed, autonomie } = params;
+function TheApp({ params, onSpeedDOWN, onSpeedUP, onTempDOWN, onTempUP }) {
+    const { speed, temp, autonomie } = params;
     return (
         <div>
-          <h1>{ autonomie }</h1>
+            <h1>
+                <small>Autonomie</small>
+                <br />
+                { autonomie | 0 } km
+            </h1>
 
-          <button onClick={ () => onSpeedDOWN() }>-</button>
-          <h4>{ speed }</h4>
-          <button onClick={ () => onSpeedUP() }>+</button>
+            <h3>
+                Vitesse
+                <br /> 
+                <button onClick={ () => onSpeedDOWN() }>-</button>
+                { speed }
+                <button onClick={ () => onSpeedUP() }>+</button>
+            </h3>
+
+            <br />
+            <h3>
+                Température
+                <br />
+                <button onClick={ () => onTempDOWN() }>-</button>
+                { temp }°
+                <button onClick={ () => onTempUP() }>+</button>
+            </h3>
         </div>
     );
 }
-
+    
 function render() {
     ReactDOM.render(
         <TheApp
             params = { store.getState().paramsState }
             onSpeedDOWN = { () => store.dispatch(doSpeedDOWN(store.getState().paramsState)) }     
-            onSpeedUP   = { () => store.dispatch(doSpeedUP(store.getState().paramsState)) }     
+            onSpeedUP   = { () => store.dispatch(doSpeedUP(store.getState().paramsState)) } 
+            
+            onTempUP    = { () => store.dispatch(doTempUP(store.getState().paramsState)) } 
+            onTempDOWN  = { () => store.dispatch(doTempDOWN(store.getState().paramsState)) } 
+            
         />,
         document.getElementById('root')
     );
