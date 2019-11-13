ef18100af1795c53e188cc9ee38491b4d010b298
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-11-13 17:14:56 +0100

    Updates

---
+++ precache-manifest.c9dd580942c95abc30c5c19fb39f94db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d22465c84bb2e25176d0",
+    "url": "/AutonomieZoe_ReactRedux/static/js/main.d22465c8.chunk.js"
+  },
+  {
+    "revision": "b2f520282435aefd8217",
+    "url": "/AutonomieZoe_ReactRedux/static/css/1.d66492b7.chunk.css"
+  },
+  {
+    "revision": "b2f520282435aefd8217",
+    "url": "/AutonomieZoe_ReactRedux/static/js/1.b2f52028.chunk.js"
+  },
+  {
+    "revision": "2933702bd93f4f2c9a7f",
+    "url": "/AutonomieZoe_ReactRedux/static/js/runtime~main.2933702b.js"
+  },
+  {
+    "revision": "edd4bf8268b4248b9995c8d166f02a60",
+    "url": "/AutonomieZoe_ReactRedux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/AutonomieZoe_ReactRedux/precache-manifest.c9dd580942c95abc30c5c19fb39f94db.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/AutonomieZoe_ReactRedux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
